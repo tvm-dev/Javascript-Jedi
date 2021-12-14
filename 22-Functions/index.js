95452d6721f6657caccb96f32fb4bdff2a7c58cd
thiago viana <tvm.dev2015@gmail.com>
2021-12-14 15:52:15 -0300

    Arrow Functions OK

---
+++ 22-Functions/index.js
@@ -0,0 +1,38 @@
+// function Hi(name) {
+//     return `Good Morning ${name}!`
+// }
+// const variable =  Hi('Thiago')
+// console.log(variable)
+
+// function sum(x = 0, y=0) {
+//   const result = (x + y)/5
+//   return result
+// }
+// console.log(sum(10, 20))
+// console.log(sum())
+
+//Const functions
+// const root = function (n){
+//   return n ** 0.5
+// }
+
+// console.log(root(9))
+// console.log(root(16))
+// console.log(root(18))
+
+
+// //Arrow functions
+// const root =  (n) => {
+//   return n ** 0.5
+// }
+
+// console.log(root(9))
+// console.log(root(16))
+// console.log(root(25))
+
+//Arrow functions
+const root = n => n ** 0.5 //Wowwwwwwwwwwwwwwwwwwwwwww
+
+console.log(root(9))
+console.log(root(16))
+console.log(root(25))
