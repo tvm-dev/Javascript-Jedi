d6c960c340b0c49dff0c3640e28823375c885114
thiago viana <tvm.dev2015@gmail.com>
2021-12-14 21:25:17 -0300

    Module 02 started Logic

---
+++ 02-Logic/01-MathOperatorsJS/index.js
@@ -0,0 +1,20 @@
+/*
+-> Operators:
+
+> bigger then
+>= bigger than or equal to
+< less then
+<= less then or equal to
+== equal //To compare only value [not used more]
+=== equal restrict
+!= different //To compare only value [not used more]
+!== different restrict (to value and type)
+*/
+let a = 10;
+let b = 5;
+let c = 2;
+let d = 2;
+
+//tests
+console.log(c > b);
+
---
+++ 02-Logic/02-LogicOperatorsJS/index.js
@@ -0,0 +1,18 @@
+/*
+Logic Operators:
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+//Tests
+  //Two true expressions:
+  //console.log(true && true); //All expressions need to be true!
+
+  //Only one expression need to return true:
+  //console.log(false || false || false || true);
+
+   //invert the expression:
+   //console.log(!false);
+    // Deny twice the expression:
+    console.log(!!true);
+
