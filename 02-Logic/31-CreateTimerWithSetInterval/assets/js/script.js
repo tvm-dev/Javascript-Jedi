1e8937cee2806f79234771fe36f60c6f290ec92c
thiago viana <tvm.dev2015@gmail.com>
2022-05-12 22:48:13 -0300

    My Timer Created

--- 02-Logic/31-CreateTimerWithSetInterval/assets/js/script.js
+++ 02-Logic/31-CreateTimerWithSetInterval/assets/js/script.js
@@ -1,9 +1,45 @@
-//Taking Values form HTML:
-const clock = document.querySelector('.clock');
-const start = document.querySelector('.start');
-const pause = document.querySelector('.pause');
-const reset = document.querySelector('.reset');
+function clock(){
+  function createSeconds(seconds){
+    const date = new Date(seconds * 1000);
+    return date.toLocaleTimeString('pt-BR', {
+      hour12: false,
+      timeZone: 'GMT'
+    });
+  }
+  
+  //Taking Values from HTML:
+  const clock = document.querySelector('.clock');
+  let seconds = 0;
+  let timer;
+  
+  function startClock(){
+    timer = setInterval(function(){
+      seconds++;
+      clock.innerHTML = createSeconds(seconds);
+    }, 1000);
+  }
+  
+  document.addEventListener('click', function(e){
+    const el = e.target;
+  
+    if (el.classList.contains('start')) {
+      clock.classList.remove('paused');
+      clearInterval(timer);
+      startClock(); 
+    }
+  
+    if (el.classList.contains('pause')) {
+      clearInterval(timer);
+    clock.classList.add('paused');
+    }
+    if (el.classList.contains('reset')) {
+      clearInterval(timer);
+      seconds = 0;
+      clock.innerHTML = '00:00:00';
+      clock.classList.remove('paused');
+    }
+  
+    });
+  }
 
-start.addEventListener('click', function(event){
-  alert('Hi, i am the Start Button!');
-});
+  clock();
